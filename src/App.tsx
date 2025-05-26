2b9692271d6f873dc58005d49f74076315cd8b57
nguyenyou <tunt081295@gmail.com>
2025-05-26 14:48:36 +0700

    🚢 ship it

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import "./App.css";
-import { Button } from "@ui5/webcomponents-react";
+import { Button, TextArea } from "@ui5/webcomponents-react";
 import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
 import { getFinalLessContent } from "./getlesssources";
+import { useState } from "react";
 
-const themeName = "pinky";
-const baseThemeName = "sap_horizon";
+const less = getFinalLessContent();
 
-const CUSTOM_THEME_METADATA = `
+function App() {
+  const [custom, setCustom] = useState(`@sapPrimary1: violet;
+@sapPrimary2: violet;
+    `);
+
+  const [customSecondary, setCustomSecondary] = useState(`@sapPrimary1: yellow;
+@sapPrimary2: yellow;
+    `);
+
+  async function compileTheme() {
+    const themeName = "pinky";
+    const baseThemeName = "sap_horizon";
+    const CUSTOM_THEME_METADATA = `
 .sapThemeMetaData-Base-baseLib {
 	background-image: url('data:text/plain;utf-8, { "Path": "Base.baseLib.${themeName}.css_variables", "Extends": ["${baseThemeName}","baseTheme"]}');
 }`;
 
-function App() {
-  async function compileTheme() {
-    const less = getFinalLessContent();
     try {
       const customLess = `
       ${less}
-
-      @sapPrimary1: violet;
-      @sapPrimary2: violet;
+      ${custom}
       `;
 
       const result = await window.less.render(customLess);
@@ -27,21 +34,84 @@ function App() {
       ${CUSTOM_THEME_METADATA}
       ${result.css}
       `;
-      // append to head style
-      const style = document.createElement("style");
-      style.textContent = customTheme;
-      document.head.appendChild(style);
+      // query the style element with id custom-theme
+      const style = document.getElementById("custom-theme");
+      if (style) {
+        style.textContent = customTheme;
+      } else {
+        const style = document.createElement("style");
+        style.id = "custom-theme";
+        style.textContent = customTheme;
+        document.head.appendChild(style);
+      }
     } catch (error) {
       console.error(error);
     }
   }
+
+  async function compileSecondaryTheme() {
+    const themeName = "custom";
+    const baseThemeName = "sap_horizon";
+    const CUSTOM_THEME_METADATA = `
+.sapThemeMetaData-Base-baseLib {
+	background-image: url('data:text/plain;utf-8, { "Path": "Base.baseLib.${themeName}.css_variables", "Extends": ["${baseThemeName}","baseTheme"]}');
+}`;
+
+    try {
+      const customLess = `
+      ${less}
+      ${customSecondary}
+      `;
+
+      const result = await window.less.render(customLess);
+      const customTheme = `
+      ${CUSTOM_THEME_METADATA}
+      ${result.css}
+      `;
+      const style = document.getElementById("custom-theme");
+      if (style) {
+        style.textContent = customTheme;
+      } else {
+        const style = document.createElement("style");
+        style.id = "custom-theme";
+        style.textContent = customTheme;
+        document.head.appendChild(style);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
       <Button design="Emphasized" onClick={() => setTheme("sap_horizon")}>
         SAP Horizon
       </Button>
-      <Button onClick={() => setTheme("pinky")}>Pinky</Button>
-      <Button onClick={compileTheme}>Compile Theme</Button>
+      <Button
+        onClick={async () => {
+          await compileTheme();
+          setTheme("pinky");
+        }}
+      >
+        Pinky
+      </Button>
+      <Button
+        onClick={async () => {
+          await compileSecondaryTheme();
+          setTheme("custom");
+        }}
+      >
+        Custom</Button>
+      <TextArea
+        rows={10}
+        value={custom}
+        onChange={(e) => setCustom(e.target.value)}
+      ></TextArea>
+      <TextArea
+        rows={10}
+        value={customSecondary}
+        onChange={(e) => setCustomSecondary(e.target.value)}
+      ></TextArea>
     </div>
   );
 }
